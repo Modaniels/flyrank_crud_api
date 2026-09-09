d32ddf8d16e3c3157812ec4693945a128bea4816
Modaniels <modaniels507@gmail.com>
2026-09-09 23:00:32 +0300

    stage 2  :read endpoints   with  404

--- src/flyrank_crud_api/main.py
+++ src/flyrank_crud_api/main.py
@@ -3,10 +3,28 @@ import  uvicorn
 
 app=FastAPI()
 
+my_tasks = [
+    {"id": 1, "title": "Task 1", "done": True},
+    {"id": 2, "title": "Task 2", "done": False},
+    {"id": 3, "title": "Task 3", "done": False},
+]
+
 
 @app.get("/")
 def hello():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+
+@app.get("/tasks")
+def get_tasks():
+    return my_tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in my_tasks:
+        if task["id"] == task_id:
+            return task
+    return {f"Task {task_id} not found"},404
 
 
 @app.get("/health")
