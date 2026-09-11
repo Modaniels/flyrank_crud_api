bc0c770b450c16f378d1edc2d407cf8b6464a24b
Modaniels <modaniels507@gmail.com>
2026-09-11 17:54:32 +0300

    Stage 4: explored SQLite

--- src/flyrank_crud_api/main.py
+++ src/flyrank_crud_api/main.py
@@ -63,6 +63,8 @@ def delete_task(task_id: int):
 def health():
     return {"status": "ok"}
 
+
+#run the app with uvicorn
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
